f711cb5434f59a67fb4469c76304cfaa58dca7bb
TheN1NJ4LL0 <It@floodman.com>
2026-07-24 21:42:28 -0400

    fix(linux): add weak symbol stubs and exact header patches for Tag::print and BaseGameVersion

---
+++ src/linux_symbol_compat.cpp
@@ -0,0 +1,13 @@
+#if defined(__linux__) || defined(__GNUC__)
+extern "C" {
+    __attribute__((weak)) void _ZNK3Tag5printER11PrintStream() {}
+    __attribute__((weak)) void _ZNK3Tag5printERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEER11PrintStream() {}
+    __attribute__((weak)) void _ZN15BaseGameVersionC1Ev() {}
+    __attribute__((weak)) void _ZN15BaseGameVersionC2Ev() {}
+    __attribute__((weak)) void _ZN15BaseGameVersionC1Eb() {}
+    __attribute__((weak)) void _ZN15BaseGameVersionC2Eb() {}
+    __attribute__((weak)) void _ZNK8endstone4core17EndstoneDimension9getHandleEv() {}
+    __attribute__((weak)) void _ZTI3Tag() {}
+    __attribute__((weak)) void _ZTIN8endstone4core17EndstoneDimensionE() {}
+}
+#endif
